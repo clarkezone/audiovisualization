b38e5091ce4bc8efd5924af60d59cd2a8a5d7b6c
jeclarke <jeclarke@JECLARKE-SP4C>
2016-09-29 16:45:16 -0700

    Add an easy test to aid with debugging

--- AudioVisualization/Views/MainPage.xaml.cs
+++ AudioVisualization/Views/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using AudioVisualization.Extensions;
+using AudioVisualization.Playback;
+using AudioVisualization.Services;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,6 +27,23 @@ namespace AudioVisualization.Views
         public MainPage()
         {
             this.InitializeComponent();
+            CreateStaticResources();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            StorageFile toneText = await StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///Assets/LevelTest_mixdown.mp3"));
+            PlayerService.Current.Playlist.Clear();
+            PlayerService.Current.Playlist.Add(await toneText.ToSong());
+
+            PlayerService.Current.Playlist.PlaybackList.MoveTo(0);
+            PlayerService.Current.Play();
+        }
+
+        void CreateStaticResources()
+        {
+            Services.PlayerService.Current.StartVisualization(win2dVisualizer);
         }
     }
 }
